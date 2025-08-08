090b6ff8c98528d7d7ca41ae89b72518bedb28f2
Léo Mindlin <hello@leomindlin.com>
2025-08-07 21:54:48 -0400

    Implement location

--- src/cards/map/MapCard.tsx
+++ src/cards/map/MapCard.tsx
@@ -1,34 +1,48 @@
 import {
   Map,
-  Source,
-  Layer,
   TerrainControl,
   Marker,
+  MarkerDragEvent,
+  LngLat,
 } from "@vis.gl/react-maplibre";
+import "maplibre-gl/dist/maplibre-gl.css"
 
 import ControlPanel from "./ControlPanel";
-import type { Terrain, Sky } from "@vis.gl/react-maplibre";
 import Pin from "./Pin";
 import { useParameterSubscription } from "@/hooks/use-parameter";
 import type { IDockviewPanelProps } from "dockview-react";
 import { MapCardParams } from "./schema";
 import { extractNumberValue,  } from "@/lib/utils";
+import { useCallback, useState } from "react";
 
-const sky: Sky = {
-  "sky-color": "#80ccff",
-  "sky-horizon-blend": 0.5,
-  "horizon-color": "#ccddff",
-  "horizon-fog-blend": 0.5,
-  "fog-color": "#fcf0dd",
-  "fog-ground-blend": 0.2,
-};
-
-const terrain: Terrain = { source: "terrain-dem", exaggeration: 1.5 };
 
 export const MapCard = ({
   params,
 }: IDockviewPanelProps<MapCardParams>) => {
   const { values } = useParameterSubscription([params.latitudeParameter, params.longitudeParameter])
+
+  const [marker, setMarker] = useState({
+    latitude: 40,
+    longitude: -100
+  });
+  const [events, logEvents] = useState<Record<string, LngLat>>({});
+
+  const onMarkerDragStart = useCallback((event: MarkerDragEvent) => {
+    logEvents(_events => ({..._events, onDragStart: event.lngLat}));
+  }, []);
+
+  const onMarkerDrag = useCallback((event: MarkerDragEvent) => {
+    logEvents(_events => ({..._events, onDrag: event.lngLat}));
+
+    setMarker({
+      longitude: event.lngLat.lng,
+      latitude: event.lngLat.lat
+    });
+  }, []);
+
+  const onMarkerDragEnd = useCallback((event: MarkerDragEvent) => {
+    logEvents(_events => ({..._events, onDragEnd: event.lngLat}));
+  }, []);
 
   return (
     <>
@@ -41,43 +55,22 @@ export const MapCard = ({
       <Map
         initialViewState={{
           latitude: 48.47614,
-          longitude: 81.32903,
+          longitude: -81.32903,
           zoom: 12,
         }}
         maxPitch={85}
         mapStyle="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
-        sky={sky}
-        terrain={terrain}
       >
-        <Source
-          id="terrain-dem"
-          type="raster-dem"
-          url="https://demotiles.maplibre.org/terrain-tiles/tiles.json"
-          tileSize={256}
-        />
-        <Source
-          id="hillshade-dem"
-          type="raster-dem"
-          url="https://demotiles.maplibre.org/terrain-tiles/tiles.json"
-          tileSize={256}
-        >
-          <Layer
-            type="hillshade"
-            layout={{ visibility: "visible" }}
-            paint={{ "hillshade-shadow-color": "#473B24" }}
-          />
-        </Source>
-
-        <Marker
-          key={`marker-rocket`}
-          latitude={45.5060}
-          longitude={73.5783}
-          anchor="bottom"
-        >
-          <div>HELLOW ORLD</div>
-        </Marker>
-
-        <TerrainControl {...terrain} position="top-left" />
+        {values[params.longitudeParameter] && values[params.latitudeParameter] && (
+          <Marker
+            key={`marker-rocket`}
+            latitude={extractNumberValue(values[params.latitudeParameter]!.engValue) ?? 48.47614}
+            longitude={extractNumberValue(values[params.longitudeParameter]!.engValue) ?? -81.32903}
+            anchor="bottom"
+          >
+            <Pin />
+          </Marker>
+        )}
       </Map>
       <ControlPanel />
     </>
