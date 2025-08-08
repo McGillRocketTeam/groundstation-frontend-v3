d70b8f78ab9a0b6aecd0d79dd5ee1f06c71ed7a3
Léo Mindlin <hello@leomindlin.com>
2025-08-07 22:28:13 -0400

    Fix Colors on CMD History

--- src/cards/command/AcknowledgementIcon.tsx
+++ src/cards/command/AcknowledgementIcon.tsx
@@ -11,11 +11,11 @@ export function AcknowledgementIcon({
   status: string | undefined;
 }) {
   if (status) {
-    if (status === "OK") return <CheckCircledIcon className="text-green-700" />;
+    if (status === "OK") return <CheckCircledIcon className="text-green-700 dark:text-green-500" />;
     else if (status === "none") {
       return <CircleIcon className="text-muted-foreground" />;
     } else {
-      return <CrossCircledIcon className="text-red-700" />;
+      return <CrossCircledIcon className="text-red-700 dark:text-red-500" />;
     }
   } else {
     return <MinusIcon className="text-muted-foreground" />;
