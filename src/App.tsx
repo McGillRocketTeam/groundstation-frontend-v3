da131478f46234b83e9699219efe38302079859c
Léo Mindlin <hello@leomindlin.com>
2025-08-07 21:26:50 -0400

    Dark Mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,29 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import RouterLayout from "@/layout/RouterLayout";
 import SettingsPage from "@/components/settings/SettingsPage";
 import TW1 from "./checklist/TW1";
+import { ThemeProvider } from "./components/ThemeProvider";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Default Route */}
-        <Route element={<RouterLayout />}>
-          <Route
-            path={""} // This will be "" for index, "critical" for critical, etc.
-            element={<DockViewLayout />}
-          />
-          <Route path="tw1" element={<TW1 />} />
-          <Route
-            path={":slug"} // This will be "" for index, "critical" for critical, etc.
-            element={<DockViewLayout />}
-          />
-          <Route path="settings" element={<SettingsPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* Default Route */}
+          <Route element={<RouterLayout />}>
+            <Route
+              path={""} // This will be "" for index, "critical" for critical, etc.
+              element={<DockViewLayout />}
+            />
+            <Route path="tw1" element={<TW1 />} />
+            <Route
+              path={":slug"} // This will be "" for index, "critical" for critical, etc.
+              element={<DockViewLayout />}
+            />
+            <Route path="settings" element={<SettingsPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
